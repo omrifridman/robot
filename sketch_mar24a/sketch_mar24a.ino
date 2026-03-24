987cc15e495594e3918802c24db210f606ee26b6
OrStern12 <Orstern1@icloud.com>
2026-03-24 14:21:26 +0200

    motors edit

---
+++ sketch_mar24a/sketch_mar24a.ino
@@ -0,0 +1,168 @@
+// -------------------------------------------------
+// Copyright (c) 2023 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+// -------------------------------------------------
+// Copyright (c) 2023 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+// -------------------------------------------------
+// Copyright (c) 2023 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+
+
+// Motor A   
+#define ENA_PIN 8 //PWM   
+#define IN1_PIN 4 
+#define IN2_PIN 5   
+// Motor B   
+#define ENB_PIN 9 //PWM   
+#define IN3_PIN 6   
+#define IN4_PIN 7   
+struct motor {   
+	 byte speed = 0;   
+	 struct {   
+	   byte input1 = LOW;   
+	   byte input2 = LOW;   
+	 } direction;   
+}; 
+
+
+
+
+
+// -------------------------------------------------
+// Copyright (c) 2023 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+
+void unsetMotorDirection(motor &motor)
+{
+  motor.direction.input1 = LOW;
+  motor.direction.input2 = LOW;
+}
+
+void setMotorDirectionForward(motor &motor)
+{
+  motor.direction.input1 = HIGH;
+  motor.direction.input2 = LOW;
+}
+
+void setMotorDirectionBackward(motor &motor)
+{
+  motor.direction.input1 = LOW;
+  motor.direction.input2 = HIGH;
+}
+
+bool isMotorDirectionUnset(motor &motor)
+{
+  return motor.direction.input1 == LOW && motor.direction.input2 == LOW;
+}
+
+bool isMotorDirectionForward(motor &motor)
+{
+  return motor.direction.input1 == HIGH && motor.direction.input2 == LOW;
+}
+
+bool isMotorDirectionBackward(motor &motor)
+{
+  return motor.direction.input1 == LOW && motor.direction.input2 == HIGH;
+}
+void sendToMotor(motor motor, int directionPin1, int directionPin2)
+{
+  // Set direction
+  digitalWrite(directionPin1, motor.direction.input1);
+  digitalWrite(directionPin2, motor.direction.input2);
+}
+
+// Send to motor without jumper
+void sendToMotor(motor motor, int speedPin, int directionPin1, int directionPin2)
+{
+  // Set speed
+  analogWrite(speedPin, motor.speed);
+
+  // Set direction
+  digitalWrite(directionPin1, motor.direction.input1);
+  digitalWrite(directionPin2, motor.direction.input2);
+}
+void setMotorSpeed(motor &motor, int speed)
+{
+  motor.speed = constrain(speed, 0, 255); // Force values between 0-255 (PWM)
+}
+
+void minMotorSpeed(motor &motor)
+{
+  setMotorSpeed(motor, 0);
+}
+
+void maxMotorSpeed(motor &motor)
+{
+  setMotorSpeed(motor, 255);
+}
+
+byte getMotorSpeed(motor &motor)
+{
+  return motor.speed;
+}
+motor motorA, motorB;   
+void setup()   
+{   
+	 Serial.begin(115200);   
+	 // Set PWM & direction pins to output for both motor   
+	 pinMode(ENA_PIN, OUTPUT);   
+	 pinMode(IN1_PIN, OUTPUT);   
+	 pinMode(IN2_PIN, OUTPUT);   
+	 pinMode(ENB_PIN, OUTPUT);   
+	 pinMode(IN3_PIN, OUTPUT);   
+	 pinMode(IN4_PIN, OUTPUT);   
+}   
+void loop()   
+{   
+	 Serial.println("Motors are stopped now");   
+	 Serial.println("Set direction FORWARD");   
+	 delay(2000);   
+	 setMotorDirectionForward(motorA);   
+	 setMotorDirectionForward(motorB);   
+	 Serial.println("Gradually increase motors speed to max");   
+	 increaseMotorsSpeed();   
+	 Serial.println("Motors are on full speed now");   
+	 delay(2000);   
+	 Serial.println("Gradually decrease motors speed to min");   
+	 decreaseMotorsSpeed();   
+	 Serial.println("Motors are stopped now");   
+	 Serial.println("Set direction BACKWARD");   
+	 delay(2000);   
+	 setMotorDirectionBackward(motorA);   
+	 setMotorDirectionBackward(motorB);   
+	 Serial.println("Gradually increase motors speed to max");   
+	 increaseMotorsSpeed();   
+	 Serial.println("Motors are on full speed now");   
+	 delay(2000);   
+	 Serial.println("Gradually decrease motors speed to min");   
+	 decreaseMotorsSpeed();   
+}   
+void sendToMotorA()   
+{   
+	 sendToMotor(motorA, ENA_PIN, IN1_PIN, IN2_PIN);   
+}   
+void sendToMotorB()   
+{   
+	 sendToMotor(motorB, ENB_PIN, IN3_PIN, IN4_PIN);   
+}   
+void increaseMotorsSpeed()   
+{   
+	 for (int speed = 0; speed <= 255; speed++) {   
+	   setMotorSpeed(motorA, speed);   
+	   setMotorSpeed(motorB, speed);   
+	   sendToMotorA();   
+	   sendToMotorB();   
+	   delay(20); // Add small delay between changes   
+	 }   
+}   
+void decreaseMotorsSpeed()   
+{   
+	 for (int speed = 255; speed >= 0; speed--) {   
+	   setMotorSpeed(motorA, speed);   
+	   setMotorSpeed(motorB, speed);   
+	   sendToMotorA();   
+	   sendToMotorB();   
+	   delay(20); // Add small delay between changes   
+	 }   
+}  
